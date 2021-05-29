2f5723f2d46f7b93f051d5a42578b6b6e37e0581
Sebastian <SebastianLSteele@knights.ucf.edu>
2021-05-29 16:09:36 -0400

    First Commit

---
+++ src/main/java/org/example/CountChars.java
@@ -0,0 +1,14 @@
+package org.example;
+import java.util.Scanner;
+
+public class CountChars {
+    public static void main(String[] args) {
+        // Let's grab a string
+        Scanner getName = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String strtocount = getName.nextLine();
+
+        // output contains string, and uses built in function.
+        System.out.println(strtocount + " has " + strtocount.length() +" characters.");
+    }
+}
